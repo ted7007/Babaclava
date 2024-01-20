8907af36bde876f8e564b43eb0af1434d27d361b
Chita <chtiastrash@yandex.r>
2024-01-20 10:24:05 +1000

    Добавил переход на страницу книги.

--- Babaclava.Web/app/src/components/BookCard/BookCard.tsx
+++ Babaclava.Web/app/src/components/BookCard/BookCard.tsx
@@ -1,13 +1,16 @@
 import { IBook } from "../../types/Book";
 import styles from "./styles.module.css";
 import bookCover from "../../assets/bookCover.jpg";
+import { Link } from "react-router-dom";
 
 export const BookCard = ({ book }: { book: IBook }) => {
   return (
-    <div className={styles.container}>
-      <img src={bookCover} alt="" />
-      <p>title</p>
-      <p className={styles.author}>author</p>
-    </div>
+    <Link to={`/${book.bookId}/type`}>
+      <div className={styles.container}>
+        <img src={bookCover} alt="" />
+        <p>title</p>
+        <p className={styles.author}>author</p>
+      </div>
+    </Link>
   );
 };
